2accc838eb32839a58d26ca4cb6b009169fa2807
AgusMaff <102746223+AgusMaff@users.noreply.github.com>
2022-12-29 21:03:58 -0300

    Update Query 4.sql
    Hice una corrección en la Query 4, me falto verificar el ano para verificar que sea el mes actual

--- MySQL/Query 4.sql
+++ MySQL/Query 4.sql
@@ -3,11 +3,11 @@
 DROP VIEW Ventas;
 */
 CREATE VIEW Ventas AS 
-SELECT id_cliente, MONTH(fecha) AS mes, SUM(cantidad) AS cantidad
+SELECT id_cliente, MONTH(fecha) AS mes, YEAR(fecha) AS ano, SUM(cantidad) AS cantidad
 FROM venta
 GROUP BY id_cliente, mes;
 
 SELECT cliente.nombre, Ventas.cantidad, Ventas.mes
 FROM cliente INNER JOIN Ventas
-ON cliente.id_cliente = Ventas.id_cliente AND mes = MONTH(CURRENT_DATE())
+ON cliente.id_cliente = Ventas.id_cliente AND mes = MONTH(CURRENT_DATE()) AND ano = YEAR(current_date())
 ORDER BY cantidad DESC;
